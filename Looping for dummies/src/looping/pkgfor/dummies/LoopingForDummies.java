4d4f756376d6c268591302f07b90db0409c4270a
User <User@LAPTOP-9G91PNNL>
2018-10-26 05:22:40 +0800

    'for' loop.

--- Looping for dummies/src/looping/pkgfor/dummies/LoopingForDummies.java
+++ Looping for dummies/src/looping/pkgfor/dummies/LoopingForDummies.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package looping.pkgfor.dummies;
+
 import java.util.Scanner;
+
 /**
  *
  * @author User
@@ -15,8 +17,9 @@ public class LoopingForDummies {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner s = new Scanner(System.in);
+
+       //FOR LOOP
+        /* Scanner s = new Scanner(System.in);
         int num;
         int start=1;
         int end=10;
@@ -26,9 +29,54 @@ public class LoopingForDummies {
         for(start=1;start<=end;start++){
             System.out.println(start + " times " + num + " = " + (ans+num));
             ans= start*num;
+        }*/
+        
+        
+        
+        
+        
+    // WHILE LOOP    
+ /*int loopval=0;
+       
+       while (loopval<4){
+           System.out.println("Print some lines");
+           loopval++;
+       }*/
+ 
+ 
+ 
+ // DO-WHILE LOOP
+ 
+ /*int loopval=0;
+ 
+ do{
+     System.out.println("Print some texts");
+     loopval++;
+ }
+ 
+ while (loopval<5);*/
+ 
+ 
+ 
+ //STATEMENTS
+ //IF STATEMENT
+ 
+/* int num;
+ Scanner s = new Scanner(System.in);
+ 
+        System.out.println("Enter a number (1-100): ");
+        num = s.nextInt();
+        
+        if(num>=1 && num<=100) {
+            System.out.println("Number is within range");
         }
-          }
-             
-        }
-    
-    
+        
+        else
+            System.out.println("Number is out of range");
+        }*/
+ 
+ 
+ 
+    }
+
+}
